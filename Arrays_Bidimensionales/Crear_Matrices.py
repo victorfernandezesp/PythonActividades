a9bbb8e6b4ad5fb55a9ebf5c6f21bc560122d691
victorfernandezesp <91657222+victorfernandezesp@users.noreply.github.com>
2022-11-14 12:21:19 +0100

    Add files via upload

--- Arrays_Bidimensionales/Crear_Matrices.py
+++ Arrays_Bidimensionales/Crear_Matrices.py
@@ -6,17 +6,19 @@
     Curso: 2022-2023
 """
 
-filas = int(input("Escribe el numero de filas:      "))
-columnas = int(input("Escribe el numero de columnas:    "))
 matriz = []
+cant_num_x = int(input("Cuantas Filas (x) va a contener el array que quieres que sume?:          "))
+cant_num_y = int(input("Cuantos Columnas (y) va a contener el array que quieres que sume?:       "))
 
-for i in range(filas):
-    matriz.append([])
-    for j in range(columnas):
-        valor = float(input("Fila {}, Columna {}:   ".format(i+1, j+1)))
-        matriz[i].append(valor)
+# Creamos la Matriz
+for x in range(cant_num_x):
+    vector = []
+    for y in range(cant_num_y):
+        auxiliar = int(input(f"Fila {x}, Columna {y}:   "))
+        vector.append(auxiliar)
+    matriz.append(vector)
 
-print()
+
 for filas in matriz:
     print("[", end=" ")
     for elemento in filas:
