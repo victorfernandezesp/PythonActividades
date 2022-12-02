8a5d0b9378a1f0c2367e9bfcb3434bf12d23106d
victorfernandezesp <91657222+victorfernandezesp@users.noreply.github.com>
2022-12-02 17:53:04 +0100

    Delete Secuenciales directory

--- Secuenciales/Ejercicio1.py
+++
@@ -1,9 +0,0 @@
-""" 1. Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
-       Este programa mediante una entrada por teclado almacena en la variable saluda un valor del nombre y luego
-       mediante un mensaje lo muestra por pantalla
-    Author: Victor Fernandez España
-    Curso:  2022-2003
-"""
-print("Este programa te saluda amistosamente")
-Saludo = str(input("¿Hola que tal, como te llamas? "))
-print("¡Hola ", Saludo, " ojala tengas un gran dia! :)")
