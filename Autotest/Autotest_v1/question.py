de234507a6700eb1cf05ee8c3dfda347e5b1378a
victorfernandezesp <91657222+victorfernandezesp@users.noreply.github.com>
2023-04-11 19:38:26 +0200

    Add files via upload

--- Autotest/Autotest_v1/question.py
+++ Autotest/Autotest_v1/question.py
@@ -80,42 +80,42 @@ class Question:
         sleep(1.25)
 
     def averigua_puntuacion_sacada_de_la_pregunta(self, elecciones_de_pregunta, respuesta):
-        puntuacion_de_eleccion = 0
+        posicion_de_la_respuesta_correcta = 0
         for j in range(len(elecciones_de_pregunta)):
             if elecciones_de_pregunta[j] == respuesta:
-                puntuacion_de_eleccion = j
+                posicion_de_la_respuesta_correcta = j
         valor_de_eleccion = []
         for i in self.__elecciones:
             valor_de_eleccion.append(i[1])
-        return self.__puntuacion * valor_de_eleccion[puntuacion_de_eleccion]
+        return self.__puntuacion * valor_de_eleccion[posicion_de_la_respuesta_correcta]
 
 
 if __name__ == '__main__':
     pr1 = Question("Planta vs Agua", "En relación con los tipos Pokemon, ¿Qué relación tiene el tipo Planta Vs Agua?",
-                   ([("Efectivo", 1), ("Normal", -0.25), ("Poco efectivo", -0.50), ("No afecta", -0.75)]), 1)
+                   ([("Muy efectivo", 1), ("Efectivo", -0.25), ("Poco efectivo", -0.50), ("No afecta", -0.75)]), 1)
     pr1.muestra_pregunta()
     pr1.respuesta_usuario()
 
     pr2 = Question("Normal vs Normal",
                    "En relación con los tipos Pokemon, ¿Qué relación tiene el tipo Normal Vs Normal?",
-                   ([("Efectivo", -0.25), ("Normal", 1), ("Poco efectivo", -0.25), ("No afecta", -0.50)]), 1)
+                   ([("Muy efectivo", -0.25), ("Efectivo", 1), ("Poco efectivo", -0.25), ("No afecta", -0.50)]), 1)
     pr2.muestra_pregunta()
     pr2.respuesta_usuario()
 
     pr3 = Question("Dragon vs Hada",
                    "En relación con los tipos Pokemon, ¿Qué relación tiene el tipo Dragon Vs Hada?",
-                   ([("Efectivo", -0.75), ("Normal", -0.25), ("Poco efectivo", 1), ("No afecta", -0.25)]), 1)
+                   ([("Muy efectivo", -0.75), ("Efectivo", -0.25), ("Poco efectivo", 1), ("No afecta", -0.25)]), 1)
     pr3.muestra_pregunta()
     pr3.respuesta_usuario()
 
     pr4 = Question("Normal vs Fantasma",
                    "En relación con los tipos Pokemon, ¿Qué relación tiene el tipo Normal vs Fantasma?",
-                   ([("Efectivo", -0.75), ("Normal", -0.50), ("Poco efectivo", -0.25), ("No afecta", 1)]), 1)
+                   ([("Muy efectivo", -0.75), ("Efectivo", -0.50), ("Poco efectivo", -0.25), ("No afecta", 1)]), 1)
     pr4.muestra_pregunta()
     pr4.respuesta_usuario()
 
     pr5 = Question("Dragon vs Dragon",
                    "En relación con los tipos Pokemon, ¿Qué relación tiene el tipo Dragon vs Dragon?",
-                   ([("Efectivo", 1), ("Normal", -0.25), ("Poco efectivo", -0.75), ("No afecta", -0.50)]), 1)
+                   ([("Muy efectivo", 1), ("Efectivo", -0.25), ("Poco efectivo", -0.75), ("No afecta", -0.50)]), 1)
     pr5.muestra_pregunta()
     pr5.respuesta_usuario()
