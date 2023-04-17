bdf466f364389cf3eaf4c60128179e3d0a217dce
victorfernandezesp <91657222+victorfernandezesp@users.noreply.github.com>
2023-04-17 20:52:55 +0200

    Add files via upload

--- Tema_09_Ficheros/Tanda3/ejercicio1/banco.py
+++ Tema_09_Ficheros/Tanda3/ejercicio1/banco.py
@@ -1,13 +1,9 @@
 """
-        Ejercicio 3:
+        Ejercicio 2:
 
-            Modifica el ejercicio de POO que gestiona una cuenta bancaria con movimientos, de forma que añadas a la
-            clase un método para guardar todos los datos de la cuenta bancaria (número, saldo y movimientos) en un
-            fichero elegido por el cliente, y un nuevo constructor que reciba como parámetro un fichero como el anterior
-            y cree el objeto con esos datos. Pruébalo con un programa.
-
-            P.D.- Usa excepciones para controlar el manejo de ficheros y en caso de no poder operar dar el mensaje de
-            error correspondiente.
+            Modifica el ejercicio de la cuenta corriente para que el método que almacena en un fichero el estado del
+            objeto guarde el objeto entero y el que lo recupera lo restaure. En esta versión no le pasamos nombre de
+            fichero al método a la hora de guardarlo, usará el número de cuenta corriente para ello.
 
     Autor: Víctor Fernández España
     Curso: 2022-2023
---
+++ Tema_09_Ficheros/Tanda3/ejercicio2/encripta_fichero.py
@@ -0,0 +1,14 @@
+"""
+        Ejercicio 3:
+
+            Modifica el ejercicio de POO que gestiona una cuenta bancaria con movimientos, de forma que añadas a la
+            clase un método para guardar todos los datos de la cuenta bancaria (número, saldo y movimientos) en un
+            fichero elegido por el cliente, y un nuevo constructor que reciba como parámetro un fichero como el anterior
+            y cree el objeto con esos datos. Pruébalo con un programa.
+
+            P.D.- Usa excepciones para controlar el manejo de ficheros y en caso de no poder operar dar el mensaje de
+            error correspondiente.
+
+    Autor: Víctor Fernández España
+    Curso: 2022-2023
+"""
