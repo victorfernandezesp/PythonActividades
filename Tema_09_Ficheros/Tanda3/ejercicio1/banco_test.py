02e15fe19bfd225505c9ea5e679a9e53bc36a6ea
victorfernandezesp <91657222+victorfernandezesp@users.noreply.github.com>
2023-04-17 20:47:24 +0200

    Add files via upload

---
+++ Tema_09_Ficheros/Tanda3/ejercicio1/banco_test.py
@@ -0,0 +1,31 @@
+"""
+        Ejercicio 3:
+
+            Modifica el ejercicio de POO que gestiona una cuenta bancaria con movimientos, de forma que añadas a la
+            clase un método para guardar todos los datos de la cuenta bancaria (número, saldo y movimientos) en un
+            fichero elegido por el cliente, y un nuevo constructor que reciba como parámetro un fichero como el anterior
+            y cree el objeto con esos datos. Pruébalo con un programa.
+
+            P.D.- Usa excepciones para controlar el manejo de ficheros y en caso de no poder operar dar el mensaje de
+            error correspondiente.
+
+    Autor: Víctor Fernández España
+    Curso: 2022-2023
+"""
+from Tema_09_Ficheros.Tanda3.ejercicio1.banco import BankAccount
+
+
+cuenta1 = BankAccount()
+cuenta2 = BankAccount(1500)
+cuenta3 = BankAccount(6000)
+cuenta1.ingresar(2000)
+cuenta1.retirar(600)
+cuenta3.ingresar(75)
+cuenta1.retirar(55)
+cuenta2.transferir(cuenta1, 100)
+cuenta1.transferir(cuenta3, 250)
+cuenta3.transferir(cuenta1, 22)
+cuenta1.convertir_objeto_a_archivo()
+cuenta2.convertir_objeto_a_archivo()
+cuenta3.convertir_objeto_a_archivo()
+
