3e4deab2cf6ba09f108112d0f546556c7eb5edcb
victorfernandezesp <91657222+victorfernandezesp@users.noreply.github.com>
2023-04-17 21:12:56 +0200

    Add files via upload

--- Tema_09_Ficheros/Tanda3/ejercicio2/encripta_fichero.py
+++ Tema_09_Ficheros/Tanda3/ejercicio2/encripta_fichero.py
@@ -1,14 +1,40 @@
 """
-        Ejercicio 3:
+        Ejercicio 2:
 
-            Modifica el ejercicio de POO que gestiona una cuenta bancaria con movimientos, de forma que añadas a la
-            clase un método para guardar todos los datos de la cuenta bancaria (número, saldo y movimientos) en un
-            fichero elegido por el cliente, y un nuevo constructor que reciba como parámetro un fichero como el anterior
-            y cree el objeto con esos datos. Pruébalo con un programa.
+            Crea un programa que encripte un fichero que le pasamos como parámetro y almacene el resultado en otro, que
+            también pasamos como parámetro, de manera que:
 
-            P.D.- Usa excepciones para controlar el manejo de ficheros y en caso de no poder operar dar el mensaje de
-            error correspondiente.
+            *   Si el programa no recibe el número de parámetros adecuado termina con un error 1.
+
+            *   Si el programa recibe un solo parámetro guardará la información encriptada en el mismo archivo del que
+                lee, pero antes advertirá al usuario de que machacará el archivo origen, dando opción a que la operación
+                no se haga.
+
+            *   Si el fichero origen no existe (da error al abrirlo como lectura) el programa termina con un mensaje de
+                error y código 2.
+
+            *   Si en el fichero destino no se puede escribir (da error al abrirlo como escritura) el programa termina
+                con un mensaje de error y código 3.
+
+            *   Para encriptar usa el método César, necesitarás una clave que debes pedir al usuario.
 
     Autor: Víctor Fernández España
     Curso: 2022-2023
 """
+import sys
+
+try:
+    nombre_archivo1 = sys.argv[1]
+except IndexError:
+    print("Parametros insuficientes o fuera de rango, debes introducir minimo 1 argumento: Nombre de archivo")
+    sys.exit(1)
+
+try:
+    nombre_archivo_final = sys.argv[2]
+except IndexError:
+    machaca_archivo = input("Machacará el archivo de origen, esta seguro? (S/N) ")
+    machaca_archivo = machaca_archivo.upper()
+    if machaca_archivo == "N":
+        pass
+
+
